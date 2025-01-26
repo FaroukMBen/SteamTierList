35eb64807dc783b82724777fbd6d4dd8d708c5ba
MixFgams <b.fmohamedse@gmail.com>
2025-01-26 15:55:31 +0100

    CHORE: Update and adapt methods for game retrieval and image generation to the latest Steam API

--- index.php
+++ index.php
@@ -6,37 +6,48 @@ $GLOBALS["gamesList"] = null;
 $GLOBALS["steamAPIKey"] = "api_key";
 
 
-function getSteamGames($steamID,$resultFormat="json"){
-    $apiBaseKey = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/";
-    $url = $apiBaseKey . "?key=" . $GLOBALS["steamAPIKey"] . "&steamid=" . $steamID . "&format=" . $resultFormat;
+function getSteamGames($steamID,$freeGamesIncluded=False){
+    $apiBaseKey = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/";
+    $url = $apiBaseKey . "?key=" . $GLOBALS["steamAPIKey"] . "&steamid=" . $steamID . "&include_appinfo=true";
     
     $curl = curl_init($url);
     curl_setopt_array($curl,[
-        CURLOPT_RETURNTRANSFER => True
+        CURLOPT_RETURNTRANSFER => True,
+        CURLOPT_SSL_VERIFYPEER => False
     ]);
     
     $response = curl_exec($curl);
+
     $response = json_decode($response,true);
+
     foreach($response["response"]["games"] as $game){
         $gamesList[] = [
             "id"=> $game["appid"], 
-            "name" => $game["name"] ?? null
+            "name" => $game["name"] ?? null,
+            "hash" => $game['img_icon_url']
         ];
+
     }
 
     return $gamesList;
 
 }
 
-function gameImagePathByID($gameID, $imgType='hero_capsule'){
-    return 'https://cdn.cloudflare.steamstatic.com/steam/apps/'. $gameID .'/'. $imgType .'.jpg';
+function gameImagePathByID($game, $imgType='header'){
+    
+    if($imgType == 'icon'){
+        return 'http://media.steampowered.com/steamcommunity/public/images/apps/'. $game['id'] .'/'. $game['hash'] .'.jpg';
+    }else{
+        return 'https://cdn.cloudflare.steamstatic.com/steam/apps/'. $game['id'] .'/'. $imgType .'.jpg';
+    }
 }
+
 
 //Getting the steam ID
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     if ($_POST["action"] = "formSteamID"){
         $steamID =$_POST["steamID"];
-        $GLOBALS["gamesList"] = getSteamGames($steamID);
+        $GLOBALS["gamesList"] = getSteamGames($steamID,True);
     }
 }
 
@@ -59,14 +70,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <h2>Your Games</h2>
             <?php
                 foreach($GLOBALS["gamesList"] as $game){
-                    $path = gameImagePathByID($game["id"]);
+                    $path = gameImagePathByID($game);
                     echo '
                     <div class="game">
                         <p>'. $game["name"] .'</p>
                         <img src="'. $path .'" alt="img'. $game["name"] .'"> 
                     </div>
                     ';
-                    
                 }
             ?>
         </section>
